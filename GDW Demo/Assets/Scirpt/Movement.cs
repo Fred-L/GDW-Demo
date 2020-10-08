f6995f190dce174a630179f3cc5c8244f8092ad1
James Pham <james.pham2@ontariotechu.net>
2020-10-08 13:31:13 -0400

    Demo

---
+++ GDW Demo/Assets/Scirpt/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    float speedcontrol = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 wasd = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.position = transform.position + wasd * Time.deltaTime * speedcontrol;
+    }
+}
